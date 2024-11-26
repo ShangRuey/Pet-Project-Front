264ef73bad4c87cd941743281c034fae8f56b006
Shang <tom830522@gmail.com>
2024-11-26 17:34:47 +0800

    feat：新增MenuItem 的onClick prop

--- src/layouts/Header/MenuItem/MenuItem.jsx
+++ src/layouts/Header/MenuItem/MenuItem.jsx
@@ -1,11 +1,16 @@
 import PropTypes from "prop-types";
 import styles from "./MenuItem.module.css";
 
-export default function MenuItem({ label, itemName }) {
-  return <div className={styles[itemName]}>{label}</div>;
+export default function MenuItem({ label, itemName, onClick }) {
+  return (
+    <div className={styles[itemName]} onClick={onClick}>
+      {label}
+    </div>
+  );
 }
 
 MenuItem.propTypes = {
   label: PropTypes.string.isRequired,
   itemName: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
