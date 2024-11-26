50b2d9ee78c78739d39e7d630287b07a56484355
Shang <tom830522@gmail.com>
2024-11-26 20:03:10 +0800

    refactor：針對Form 及 Login.jsx 做程式碼移動

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,30 +1,15 @@
-import Title from "../../components/Title/Title.jsx";
-import Label from "../../components/Label/Label.jsx";
-import Input from "../../components/Input/Input.jsx";
-import Button from "../../components/Button/Button.jsx";
 import styles from "./Form.module.css";
+import PropTypes from "prop-types";
 
-export default function Form({ ...props }) {
+export default function Form({ children, handleSubmit }) {
   return (
-    <form className={styles.setForm} {...props}>
-      <Title label="會員登入" />
-      <Label label="帳號" />
-      <Input
-        type="text"
-        maxLength="14"
-        minLength="4"
-        pattern="[A-Za-z0-9]+"
-        required
-      />
-      <Label label="密碼" />
-      <Input
-        type="password"
-        maxLength="16"
-        pattern="[A-Za-z0-9]+"
-        minLength="4"
-        required
-      />
-      <Button label="登入" type="submit" />
+    <form className={styles.setForm} onSubmit={handleSubmit}>
+      {children}
     </form>
   );
 }
+
+Form.propTypes = {
+  children: PropTypes.node.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
