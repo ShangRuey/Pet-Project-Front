690af05c77da81ad4c1cb93cd764e2cc3ebd2abe
Shang <tom830522@gmail.com>
2024-12-04 15:56:04 +0800

    feat：在App.jsjx 新增 CartProvider 及 Detail 路由

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,10 @@ import Member from "./pages/Member/Member.jsx";
 import Map from "./pages/Map/Map.jsx";
 import Community from "./pages/Community/Community.jsx";
 import Adopt from "./pages/Adopt/Adopt.jsx";
+import Detail from "./pages/Detail/Detail.jsx";
 import UpdatePassword from "./pages/UpdatePassword/UpdatePassword.jsx";
 import { UserProvider } from "./contexts/UserContext";
+import { CartProvider } from "./contexts/CartContext"; // 添加這行
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -52,70 +54,76 @@ function App() {
 
   return (
     <UserProvider>
-      <Router>
-        <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-        <Main>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                isLoggedIn ? (
-                  <Navigate to="/home" />
-                ) : (
-                  <Login setIsLoggedIn={setIsLoggedIn} />
-                )
-              }
-            />
-            <Route
-              path="/register"
-              element={isLoggedIn ? <Navigate to="/home" /> : <Register />}
-            />
-            <Route
-              path="/home"
-              element={isLoggedIn ? <Home /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/shop"
-              element={isLoggedIn ? <Shop /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/cart"
-              element={isLoggedIn ? <Cart /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/checkout"
-              element={isLoggedIn ? <Checkout /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/member"
-              element={
-                isLoggedIn ? (
-                  <Member setIsLoggedIn={setIsLoggedIn} />
-                ) : (
-                  <Navigate to="/" />
-                )
-              }
-            />
-            <Route
-              path="/map"
-              element={isLoggedIn ? <Map /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/community"
-              element={isLoggedIn ? <Community /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/adopt"
-              element={isLoggedIn ? <Adopt /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/update-password"
-              element={<UpdatePassword setIsLoggedIn={setIsLoggedIn} />}
-            />
-          </Routes>
-        </Main>
-        <Footer />
-      </Router>
+      <CartProvider>
+        <Router>
+          <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <Main>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  isLoggedIn ? (
+                    <Navigate to="/home" />
+                  ) : (
+                    <Login setIsLoggedIn={setIsLoggedIn} />
+                  )
+                }
+              />
+              <Route
+                path="/register"
+                element={isLoggedIn ? <Navigate to="/home" /> : <Register />}
+              />
+              <Route
+                path="/home"
+                element={isLoggedIn ? <Home /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/shop"
+                element={isLoggedIn ? <Shop /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/cart"
+                element={isLoggedIn ? <Cart /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/checkout"
+                element={isLoggedIn ? <Checkout /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/member"
+                element={
+                  isLoggedIn ? (
+                    <Member setIsLoggedIn={setIsLoggedIn} />
+                  ) : (
+                    <Navigate to="/" />
+                  )
+                }
+              />
+              <Route
+                path="/map"
+                element={isLoggedIn ? <Map /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/community"
+                element={isLoggedIn ? <Community /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/adopt"
+                element={isLoggedIn ? <Adopt /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/update-password"
+                element={<UpdatePassword setIsLoggedIn={setIsLoggedIn} />}
+              />
+              <Route
+                path="/detail/:id"
+                element={isLoggedIn ? <Detail /> : <Navigate to="/" />}
+              />
+            </Routes>
+          </Main>
+          <Footer />
+        </Router>
+      </CartProvider>
     </UserProvider>
   );
 }
