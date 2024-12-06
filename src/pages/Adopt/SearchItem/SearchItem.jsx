edba37a99494f34a12746be5a41d219129c84487
Shang <tom830522@gmail.com>
2024-12-06 12:05:15 +0800

    feat：SearchItem.jsx 新增onChange 屬性

--- src/pages/Adopt/SearchItem/SearchItem.jsx
+++ src/pages/Adopt/SearchItem/SearchItem.jsx
@@ -1,11 +1,14 @@
 import styles from "./SearchItem.module.css";
 import PropTypes from "prop-types";
 
-export default function SearchItem({ label, options }) {
+export default function SearchItem({ label, options, onChange }) {
   return (
     <div className={styles.searchItem}>
       <span>{label}</span>
-      <select className={styles.searchSelect}>
+      <select
+        className={styles.searchSelect}
+        onChange={(e) => onChange(e.target.value)}
+      >
         <option value="">請選擇</option>
         {options.map(([key, value], index) => (
           <option key={index} value={key}>
@@ -20,4 +23,5 @@ export default function SearchItem({ label, options }) {
 SearchItem.propTypes = {
   label: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(PropTypes.array).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
